f130ca032c5d5232c9ff8580cee08379d1ee3857
Calum <calumgcoppack@gmail.com>
2019-01-07 18:53:49 -0500

    Added modules folder and a testing module with ping command

---
+++ Kamtro Bot/Kamtro Bot/Modules/TestingModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace Kamtro_Bot.Modules
+{
+    /// <summary>
+    /// This module will contain some testing commands
+    /// </summary>
+    [Name("Testing")]
+    class TestingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Name("Ping")]
+        [Summary("Simple ping command, bot responds with pong.")]
+        public async Task PingAsync() {
+            await ReplyAsync("Pong!");
+        }
+    }
+}
