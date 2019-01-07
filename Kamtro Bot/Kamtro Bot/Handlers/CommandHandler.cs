55f669a181ca63cf9e40124bffbd0dce91786a90
Calum <calumgcoppack@gmail.com>
2019-01-06 21:13:24 -0500

    Hopefully added service provider

--- Kamtro Bot/Kamtro Bot/Handlers/CommandHandler.cs
+++ Kamtro Bot/Kamtro Bot/Handlers/CommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,8 @@ namespace Kamtro_Bot.Handlers
         private IServiceProvider _provider;
         
         public CommandHandler() {
-
+            _provider = new ServiceCollection().AddSingleton(_client).AddSingleton(this).BuildServiceProvider();  // WARNING: MAY BE BROKEN
+            instance = this;
         }
 
         public async Task HandleCommandAsync(SocketMessage m) {
@@ -33,12 +35,12 @@ namespace Kamtro_Bot.Handlers
 
             int argPos = 0;  // I never really knew what this did, but it's necessary or everything dies -C
             if(message.HasStringPrefix(Program.Settings.prefix, ref argPos)) {
-                // if it's a command
+                // if it's a command  -C
                 SocketCommandContext context = new SocketCommandContext(_client, message);
                 IResult result = await _service.ExecuteAsync(context, argPos, _provider);
 
                 if(!result.IsSuccess && result.Error != CommandError.UnknownCommand) {
-                    // if there was an error with the valid command
+                    // if there was an error with the valid command  -C
                     await message.Channel.SendMessageAsync($"An error occured! {result.ErrorReason}");
                 }
             }
