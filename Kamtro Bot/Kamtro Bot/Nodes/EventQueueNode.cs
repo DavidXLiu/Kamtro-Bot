b5b33aecafbe0beb575156b76989ee8eed18270f
Calum <calumgcoppack@gmail.com>
2019-01-14 22:13:43 -0500

    Added the EventQueueNode class.

---
+++ Kamtro Bot/Kamtro Bot/Nodes/EventQueueNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kamtro_Bot.Interfaces;
+
+namespace Kamtro_Bot.Nodes
+{
+    /// <summary>
+    /// Node for the event queue.
+    /// </summary>
+    /// -C
+    public class EventQueueNode
+    {
+        public DateTime TimeCreated;  // This is for the timeout and garbage collection thread
+        public KamtroEmbedBase EventAction;  // This is the action that the node is for
+
+        public EventQueueNode(KamtroEmbedBase action) {
+            EventAction = action;
+        }
+    }
+}
