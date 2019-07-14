29838ad89d8e4c09854fa2be60d27437eeba7185
Calum <calumgcoppack@gmail.com>
2019-07-14 00:11:00 -0400

    Fixed user data node, and added user inventory

--- Kamtro Bot/Kamtro Bot/Nodes/UserDataNode.cs
+++ Kamtro Bot/Kamtro Bot/Nodes/UserDataNode.cs
@@ -15,17 +15,21 @@ namespace Kamtro_Bot.Nodes
         public string Username;
         public int Score;  // Message score  -C
         public int Reputation;  // Reputation points  -C
+        public int ReputationToGive; // Rep they have left to give to others
         public int Money;  // Kamtrokens  -C
         public int CurrentTitle;  // The id of user's selected title.  -C
         public List<int> Titles;  // A list of title ids the user has  -C
-        public int Strikes;  // The number of strikes a user has  -C
+        public int Strikes;  // The number of strikes a user has. (This might end up getting removed, since the strike system is already in place) -C
         public bool Nsfw;  // if the user has access to NSFW
 
-        public UserDataNode(string username, int score = 0, int rep = 0, int strikes = 0, bool nsfw = true) {
+        public UserInventoryNode Inventory;
+
+        public UserDataNode(string username, int score = 0, int rep = 0, int repg = 3, int strikes = 0, bool nsfw = true) {
             Username = username;
 
             Score = score;
             Reputation = rep;
+            ReputationToGive = repg;
             Strikes = strikes;
             Nsfw = nsfw;
         }
---
+++ Kamtro Bot/Kamtro Bot/Nodes/UserInventoryNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamtro_Bot.Nodes
+{
+    /// <summary>
+    /// Node containing data on the user's invnetory
+    /// (I think this was on Retropolis)
+    /// To Be Implemented, currently here for a field in <see cref="UserDataNode"/>
+    /// </summary>
+    public class UserInventoryNode
+    {
+        public UserInventoryNode() {
+
+        }
+    }
+}
