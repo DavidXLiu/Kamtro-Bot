cc32de5704fc2a4979f8b7344610c2831c989172
Calum <calumgcoppack@gmail.com>
2019-08-27 20:57:38 -0400

    Admin users can now use commands anywhere, and the admin channel can now be used as the bot channel

--- Kamtro Bot/Kamtro Bot/Handlers/CommandHandler.cs
+++ Kamtro Bot/Kamtro Bot/Handlers/CommandHandler.cs
@@ -88,7 +88,7 @@ namespace Kamtro_Bot.Handlers
             //////////////////////////////////////
             /// Command Checks Here
             //////////////////////////////////////
-            if (message.HasStringPrefix(Program.Settings.Prefix, ref argPos) && (message.Channel.Id == Program.Settings.BotChannelID || (message.Channel is IPrivateChannel && ServerData.Server.GetUser(message.Author.Id) != null))) {                
+            if (message.HasStringPrefix(Program.Settings.Prefix, ref argPos) && (message.Channel.Id == Program.Settings.BotChannelID || message.Channel.Id == Program.Settings.AdminChannelID || (ServerData.Server.GetUser(message.Author.Id) != null && ServerData.HasPermissionLevel(ServerData.Server.GetUser(message.Author.Id), ServerData.PermissionLevel.ADMIN)) || (message.Channel is IPrivateChannel && ServerData.Server.GetUser(message.Author.Id) != null))) {                
                 // if it's a command in the right channel or a DM  -C
                 SocketCommandContext context = new SocketCommandContext(_client, message);
                 IResult result = await _service.ExecuteAsync(context, argPos, _provider);
