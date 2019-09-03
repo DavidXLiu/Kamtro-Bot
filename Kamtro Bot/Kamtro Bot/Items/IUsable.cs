cec5ab6578b4ffc1b4b1aa894afc6171f4c95d99
Calum <calumgcoppack@gmail.com>
2019-09-02 20:12:10 -0400

    Started work on items

---
+++ Kamtro Bot/Kamtro Bot/Items/IUsable.cs
@@ -0,0 +1,14 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamtro_Bot.Items
+{
+    public interface IUsable
+    {
+        void Use(SocketGuildUser user, params object[] args);
+    }
+}
---
+++ Kamtro Bot/Kamtro Bot/Items/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamtro_Bot.Items
+{
+    public class Item {
+        #region Enums
+        public enum ItemRarity {
+            COMMON,
+            UNCOMMON,
+            RARE,
+            EPIC,
+            LEGENDARY
+        }
+        #endregion
+
+        private string Name;
+        private ItemRarity Rarity;
+        private string Description;
+        private int DefaultSellPrice;
+
+        private Dictionary<int, int> Recipe;
+
+        public Item(string name, ItemRarity rarity) {
+            Name = name;
+            Rarity = rarity;
+            Description = desc;
+
+
+        }
+
+
+        public int GetSellPrice() {
+            // TODO
+            return 0;
+        }
+    }
+}
