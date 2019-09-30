83b4d0a0a8489843c198886a797e3c444bd52c19
Calum <calumgcoppack@gmail.com>
2019-09-29 21:30:08 -0400

    Added stubs for ItemModule commands

---
+++ Kamtro Bot/Kamtro Bot/Modules/ItemModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace Kamtro_Bot.Modules
+{
+    public class ItemModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("shop")]
+        public async Task ShopAsync([Remainder] string args = "") {
+
+        }
+
+        [Command("craft")]
+        public async Task CraftAsync([Remainder] string args = "") {
+
+        }
+
+        [Command("inventory")]
+        public async Task InventoryAsync([Remainder] string args = "") {
+
+        }
+    }
+}
