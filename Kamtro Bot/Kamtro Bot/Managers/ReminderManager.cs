3ec41361e4e2eb7d606336153acbdbc37f05b25e
Calum <calumgcoppack@gmail.com>
2019-12-20 20:32:03 -0500

    Started adding reminders

---
+++ Kamtro Bot/Kamtro Bot/Managers/ReminderManager.cs
@@ -0,0 +1,22 @@
+﻿using Kamtro_Bot.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamtro_Bot.Managers
+{
+    public static class ReminderManager
+    {
+        public static Dictionary<ulong, List<ReminderNode>> Reminders;
+
+        public static void LoadReminders() {
+
+        }
+
+        public static void SaveReminders() {
+            // TODO: SAVE REMINDERS
+        }
+    }
+}
---
+++ Kamtro Bot/Kamtro Bot/Nodes/ReminderNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamtro_Bot.Nodes
+{
+    public class ReminderNode
+    {
+        public string Name;
+        public DateTime Date;
+        public string Description;
+
+        public ReminderNode(string name, DateTime date, string desc) {
+            Name = name;
+            Date = date;
+            Description = desc;
+        }
+
+        /// <summary>
+        /// Gets the time until the reminder needs to be sent
+        /// </summary>
+        /// <returns>A TimeSpan object representing the time until the user must be messages</returns>
+        public TimeSpan GetTimeUntilRemind() {
+            return Date - DateTime.UtcNow;
+        }
+    }
+}
