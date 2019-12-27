26bd1a937cfe12f868df26d165cb6d2cb0ca56ab
Calum <calumgcoppack@gmail.com>
2019-12-27 15:17:01 -0500

    Added TImeZoneNode

---
+++ Kamtro Bot/Kamtro Bot/Nodes/TimeZoneNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamtro_Bot.Nodes
+{
+    public class TimeZoneNode
+    {
+        public int Hour;
+        public int Minute;
+
+        public TimeZoneNode(int h, int m) {
+            Hour = h;
+            Minute = m;
+        }
+    }
+}
