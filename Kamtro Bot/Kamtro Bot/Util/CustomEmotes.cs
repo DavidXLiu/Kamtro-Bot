9140f6ac3112c8e146bb8a3a11a2a90cea55de4f
Calum <calumgcoppack@gmail.com>
2019-12-31 14:30:21 -0500

    Fixed emotes

--- Kamtro Bot/Kamtro Bot/Util/CustomEmotes.cs
+++ Kamtro Bot/Kamtro Bot/Util/CustomEmotes.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Kamtro_Bot
 {
-    public class CustomEmotes
+    public static class CustomEmotes
     {
-        //public const string CursorAnimated = "<a:Cursor:397477460125679618>";
-        //public const string CursorBlankSpace = "<:__:396345469267083274>";
-        public const string CursorAnimated = "<a:Cursor:612352043310514176>"; // Testing
-        public const string CursorBlankSpace = "<:Blank:612352610409644067>"; // Testing
+        public const string CursorAnimated = "<a:Cursor:397477460125679618>";
+        public const string CursorBlankSpace = "<:__:396345469267083274>";
+        //public const string CursorAnimated = "<a:Cursor:612352043310514176>"; // Testing
+        //public const string CursorBlankSpace = "<:Blank:612352610409644067>"; // Testing
         public const string Arcy = "<:arcy:396886267704508428>";
 
         public static string[] porterEmojis = { "<:Porter1:451190931513802782>", "<:Porter2:451193390349156363>" };
