693174ffbe3e6389acc2d2cf8f708e78a087e18b
Calum <calumgcoppack@gmail.com>
2019-12-31 14:23:41 -0500

    Added legacy score

--- Kamtro Bot/Kamtro Bot/Interfaces/BasicEmbeds/ProfileEmbed.cs
+++ Kamtro Bot/Kamtro Bot/Interfaces/BasicEmbeds/ProfileEmbed.cs
@@ -46,6 +46,7 @@ namespace Kamtro_Bot.Interfaces.BasicEmbeds
             eb.AddField("Total Activity Rating", Data.Score.ToString(), true);
             eb.AddField("Kamtrokens", Data.Kamtrokens, true); 
             eb.AddField("Reputation Score", Data.Reputation.ToString(), true);
+            eb.AddField("Legacy Score", Data.LegacyScore.ToString(), true);
             eb.AddField("Max Reputation per Week", Data.MaxReputation, true);
             eb.AddField("Titles Obtained", GetTitleList(), true); 
 
